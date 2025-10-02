aa52da69afd30f1807a613271649b545aaebc870
jc3482 <jc3482@georgetown.edu>
2025-10-02 03:13:21 +0000

    Deploying to gh-pages from @ staccjch/staccjch.github.io@f1f3ab5ced9a478739adbcee2f476d9cb453d3ff 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "staccjch/staccjch.github.io",
+    "data-repo": "",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
