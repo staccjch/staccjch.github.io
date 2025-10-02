df15117fca6cbe5761414f10eaffc526f338bb28
jc3482 <jc3482@georgetown.edu>
2025-10-02 03:18:38 +0000

    Deploying to gh-pages from @ staccjch/staccjch.github.io@5be41abd9970d7704515e52437f1c6db86b6a22f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,13 +4,13 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about",
-    title: "about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/al-folio/";
     },
   },{id: "nav-about",
-          title: "about",
+          title: "About",
           description: "",
           section: "Navigation",
           handler: () => {
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/al-folio/cv/";
           },
         },{id: "nav-projects",
-          title: "projects",
+          title: "Projects",
           description: "A growing collection of your cool projects.",
           section: "Navigation",
           handler: () => {
